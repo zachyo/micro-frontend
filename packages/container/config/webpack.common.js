988592e88cbcdbe55937051e6cd51ea96920fc64
zarkie <segunzacheusi@gmail.com>
2025-01-23 14:52:13 +0100

    chore: set up webpack prod config for container and marketing

--- packages/container/config/webpack.common.js
+++ packages/container/config/webpack.common.js
@@ -1,18 +1,25 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-    module : {
-        rules : [
-            {
-                // file with extension .mjs or .js to be processed by babel-loader
-                test : /\.m?js$/,
-                exclude : /node_modules/,
-                use : {
-                    loader : 'babel-loader',
-                    options : {
-                        presets : ['@babel/preset-react','@babel/preset-env'],
-                        plugins : ['@babel/plugin-transform-runtime']
-                    }
-                }
-            }
-        ]
-    }
-}
+  module: {
+    rules: [
+      {
+        // file with extension .mjs or .js to be processed by babel-loader
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+            plugins: ["@babel/plugin-transform-runtime"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
---
+++ packages/marketing/config/webpack.prod.js
@@ -0,0 +1,22 @@
+const { merge } = require("webpack-merge");
+const commonConfig = require("./webpack.common.js");
+const packageJson = require("../package.json");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+const prodConfig = {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "marketing",
+      exposes: {
+        "./MarketingApp": "./src/bootstrap",
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
